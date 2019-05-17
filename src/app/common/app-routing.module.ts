78022d9c686a56fe47980332018ed509f33f2b65
mariaantony.gnanasekaran <mariaantony.gnanasekaran@syncfusion.com>
2019-05-17 17:39:03 +0530

    testing

--- src/app/common/app-routing.module.ts
+++ src/app/common/app-routing.module.ts
@@ -1,26 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { Type, ModuleWithProviders, NgModule } from '@angular/core';
-import { EJ_REPORTVIEWER_COMPONENTS } from '@syncfusion/reporting-angular/src/ej/reportviewer.component';
-import { EJ_REPORTDESIGNER_COMPONENTS } from '@syncfusion/reporting-angular/src/ej/reportdesigner.component';
+import { EJ_REPORTVIEWER_COMPONENTS } from '@syncfusion/reporting-angular/src/ej/reportviewer.component'
+import { EJ_REPORTDESIGNER_COMPONENTS } from '@syncfusion/reporting-angular/src/ej/reportdesigner.component
 import { Routes, RouterModule } from '@angular/router';
-import { HelloComponent } from '../components/hello/hello.component';
-import { ReportComponent } from '../components/report/report.component';
-
-const components: any[] | Type<any> | ModuleWithProviders<{}> = [
+import { GettingStarted } from '../components/getting-started/getting-started.component';
+const components: any[] | Type<any> | ModuleWithProviders<{}> = [
   EJ_REPORTVIEWER_COMPONENTS,
   EJ_REPORTDESIGNER_COMPONENTS,
-  HelloComponent,
-  ReportComponent
+  GettingStarted
 ];
-
-const routes: Routes = [
-  { path: '', redirectTo: 'hello', pathMatch: 'full' },
-  { path: 'hello', component: HelloComponent },
-  { path: 'report', component: ReportComponent },
-
+const routes: Routes = [
+  { path: '', redirectTo: 'getting-started', pathMatch: 'full' },
+  { path: 'getting-started', component: GettingStarted },
+  { path: '**', redirectTo: 'getting-started' }
 ];
-
-@NgModule({
+@NgModule({
   declarations: components,
   imports: [
     BrowserModule,
---
+++ src/app/components/getting-started/app.component.ts
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'reportviewerapp';
+  public serviceUrl: string;  
+  public reportPath: string;
+
+    constructor() {
+        this.serviceUrl = '{{site.reportserviceurl}}api/ReportViewer';
+        this.reportPath = '~/Resources/docs/sales-order-detail.rdl';
+    }
+}
